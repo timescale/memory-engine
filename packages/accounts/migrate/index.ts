b8707e10e89ff16ddb9c9736823c863c1a199889
John Pruitt <jgpruitt@gmail.com>
2026-04-06 10:00:24 -0500

    feat(accounts): add public API exports

---
+++ packages/accounts/migrate/index.ts
@@ -0,0 +1,4 @@
+export type { MigrateResult } from "./runner";
+export { dryRun, getMigrations, getVersion, migrate } from "./runner";
+export type { AccountsConfig, ResolvedConfig } from "./template";
+export { defaultConfig, resolveConfig, template } from "./template";
