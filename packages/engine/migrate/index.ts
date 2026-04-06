ea54eb8dd8d79d48d62f0ec16e004d7dfa308f65
John Pruitt <jgpruitt@gmail.com>
2026-04-06 10:30:10 -0500

    feat(engine): export getVersion from migrate module

--- packages/engine/migrate/index.ts
+++ packages/engine/migrate/index.ts
@@ -10,6 +10,12 @@ export {
 export type { ProvisionResult } from "./provision";
 export { provisionEngine } from "./provision";
 export type { MigrateResult } from "./runner";
-export { dryRun, getMigrations, migrateAll, migrateEngine } from "./runner";
+export {
+  dryRun,
+  getMigrations,
+  getVersion,
+  migrateAll,
+  migrateEngine,
+} from "./runner";
 export type { EngineConfig, ResolvedConfig } from "./template";
 export { defaultConfig, resolveConfig, template } from "./template";
