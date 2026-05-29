3086657c04c9af33900c2de5265729b5ce70b9c8
John Pruitt <jgpruitt@gmail.com>
2026-05-29 09:39:32 -0500

    simplify principal name indexes

--- packages/core/migrate/incremental/002_principal.sql
+++ packages/core/migrate/incremental/002_principal.sql
@@ -30,6 +30,6 @@ create table core.principal
 -- users must have a globally unique name
 create unique index on core.principal (name) include (user_id) where user_id is not null;
 -- each user's agents must have a unique name (per that user)
-create unique index on core.principal (owner_id, name) include (agent_id) where agent_id is not null;
+create unique index on core.principal (owner_id, name) where agent_id is not null;
 -- each space's groups must have a unique name (per that space)
-create unique index on core.principal (space_id, name) include (group_id) where group_id is not null;
+create unique index on core.principal (space_id, name) where group_id is not null;
