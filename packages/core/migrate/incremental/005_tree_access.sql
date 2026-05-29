91b9a9d6f59ec1b7f43b0a02eb03c98cb9ed6889
John Pruitt <jgpruitt@gmail.com>
2026-05-29 09:24:41 -0500

    chore: reduce indexes on core.tree_access

--- packages/core/migrate/incremental/005_tree_access.sql
+++ packages/core/migrate/incremental/005_tree_access.sql
@@ -8,8 +8,5 @@ create table core.tree_access
 , access int not null check (access in (1, 2, 3)) -- 1 = read, 2 = write, 3 = owner
 , created_at timestamptz not null default now()
 , updated_at timestamptz
-, unique (principal_id, space_id, tree_path) include (access)
+, unique (space_id, principal_id, tree_path) include (access)
 );
-
--- list access per space or per space and principal
-create index on core.tree_access (space_id, principal_id);
